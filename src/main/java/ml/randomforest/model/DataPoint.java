4f6ceb7183298d5433ae600540bc5fd5c2fded36
saerxiao <sa.xiao2009@gmail.com>
2016-07-03 23:08:05 -0700

    Replacing tabs with spaces

--- src/main/java/ml/randomforest/model/DataPoint.java
+++ src/main/java/ml/randomforest/model/DataPoint.java
@@ -5,23 +5,23 @@ package ml.randomforest.model;
  */
 public class DataPoint {
 
-	private int label = -1;
-	private float[] features;
-	
-	public DataPoint(float[] features) {
-		this.features = features;
-	}
-	
-	public DataPoint(int label, float[] features) {
-		this(features);
-		this.label = label;
-	}
-	
-	public int getLabel() {
-		return label;
-	}
-	
-	public float[] getFeatures() {
-		return features;
-	}
+    private int label = -1;
+    private float[] features;
+
+    public DataPoint(float[] features) {
+        this.features = features;
+    }
+
+    public DataPoint(int label, float[] features) {
+        this(features);
+        this.label = label;
+    }
+
+    public int getLabel() {
+        return label;
+    }
+
+    public float[] getFeatures() {
+        return features;
+    }
 }
